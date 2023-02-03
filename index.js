ede2d3135d60a662ca071186d1b8d28c717de536
Thomas <thomasjohnso@gmail.com>
2023-02-03 09:55:34 -0500

    finished with game, everything is working

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 let score = 0;
+const scoreDisplay = document.getElementById("score");
+
+const holes = document.getElementsByClassName("hole");
+
+function moleA() {
+  const randomHoleIndex = Math.floor(Math.random() * holes.length);
+  holes[randomHoleIndex].classList.toggle("mole");
+}
+
+setInterval(moleA, 300);
+
+const gameArea = document.getElementById("whack-a-mole");
+gameArea.addEventListener("click", function (clickEvent) {
+  if (clickEvent.target.matches(".mole")) {
+    score++;
+    clickEvent.target.classList.remove("mole");
+    scoreDisplay.innerText = score;
+  }
+});
